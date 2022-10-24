7443759606f1e5b55540c0cab300d7b55ae7c094
임상은 <sang9203@naver.com>
2022-10-24 14:34:22 +0900

    [FE] feat: Router 구축

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -1,14 +1,20 @@
-/* eslint-disable import/no-unresolved */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from 'pages/Home';
+import CheerPage from 'pages/CheerPage';
+import EventPage from 'pages/EventPage';
+import NavBar from 'components/NavBar';
 import Error from 'pages/Error';
 
 function AppRouter(): JSX.Element {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path="/error" element={<Error />} />
-        {/* 나중에 path "*" 로 변경 */}
+        <Route path="/" element={<Home />} />
+        <Route path="/cheer/:userUrl" element={<CheerPage />} />
+        <Route path="/event" element={<EventPage />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
