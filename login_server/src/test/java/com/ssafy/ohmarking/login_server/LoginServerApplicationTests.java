9d1642f5fa9f87b0939a4968758e9265c73c2bce
김대현 <qpwo943@naver.com>
2022-10-27 16:31:55 +0900

    [BE] Login Server 초기 세팅

---
+++ login_server/src/test/java/com/ssafy/ohmarking/login_server/LoginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ohmarking.login_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
