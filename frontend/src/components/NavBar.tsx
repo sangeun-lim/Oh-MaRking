7050f8bef11201976cc228579268da1a5a5bf09e
sjhyun1006 <sjhyun1006@naver.com>
2022-11-01 09:22:40 +0900

    [FE] fix : navbar space-between

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -24,7 +24,8 @@ function NavBar(): JSX.Element {
 
   return (
     <Navbar className={styles.navList} expand="xl">
-      <Container>
+      {/* <Container className={styles.box_container}> */}
+      <div className={styles.box_container}>
         <Navbar.Brand className={styles.left} href="/">
           <img
             src="https://firebasestorage.googleapis.com/v0/b/stately-century-349715.appspot.com/o/character1.png?alt=media&token=e6eb279b-6fe1-441a-9a81-b065c24c9f4b"
@@ -35,8 +36,12 @@ function NavBar(): JSX.Element {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="/cheer/wfaw">내 응원가기</Nav.Link>
-            <Nav.Link href="/event">이벤트 보러가기</Nav.Link>
+            <Nav.Link className={styles.nav_font} href="/cheer/wfaw">
+              내 응원가기
+            </Nav.Link>
+            <Nav.Link className={styles.nav_font} href="/event">
+              이벤트 보러가기
+            </Nav.Link>
             <Nav.Link href="/event">
               <img
                 src="https://firebasestorage.googleapis.com/v0/b/stately-century-349715.appspot.com/o/kakao_login_medium_narrow.png?alt=media&token=83a37f58-dce3-4758-bfcf-1ddd7699349f"
@@ -46,7 +51,8 @@ function NavBar(): JSX.Element {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+      </div>
+      {/* </Container> */}
     </Navbar>
     // <div>
     //   <div className={styles.navList}>
