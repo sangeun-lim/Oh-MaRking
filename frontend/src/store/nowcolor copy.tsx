283d137b69fc2afa2a4313fee178530efebdaf21
노은영 <ndh0210@naver.com>
2022-11-01 09:47:35 +0900

    [FE] feat : redux color 추가

---
+++ frontend/src/store/nowcolor copy.tsx
@@ -0,0 +1,16 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// // 초기값
+// const initialState = { : false };
+
+// export const nowColorReducer = createSlice({
+//   name: 'nowColor',
+//   initialState: initialState ,
+//   reducers: {
+//     chageNowColor: (state) => {
+//       state.nowColor = !state.modal;
+//     },
+//   },
+// });
+// export const { chageNowColor } = nowColorReducer.actions;
+// export default nowColorReducer.reducer;
