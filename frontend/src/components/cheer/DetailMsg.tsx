47bcdf64f8652cf3bd018522540fb73304886f14
임상은 <sang9203@naver.com>
2022-11-02 17:59:54 +0900

    [FE] feat: detailmsg 구성중

---
+++ frontend/src/components/cheer/DetailMsg.tsx
@@ -0,0 +1,198 @@
+import React, { Dispatch, useState } from 'react';
+import { NoteDetail, EditNote } from 'utils/Interface';
+import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import styles from './CreateMsg.module.scss';
+import '../../style/style.scss';
+
+interface Props {
+  pass: boolean;
+  setPass: Dispatch<React.SetStateAction<boolean>>;
+  formData: NoteDetail;
+}
+
+function DetailMsg({ pass, setPass, formData }: Props): JSX.Element {
+  const [onEdit, setOnEdit] = useState<boolean>(false);
+  const [editMsg, setEditMsg] = useState<EditNote>({
+    nickname: formData.nickname,
+    content: formData.content,
+    show_date: formData.show_date,
+    date: formData.date,
+    problem_num: formData.problem_num,
+    check_num: formData.check_num,
+  });
+
+  const onChange = (e: any) => {
+    const { name, value } = e.target;
+
+    setEditMsg((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+  };
+
+  const onEditClick = () => {
+    setOnEdit(!onEdit);
+  };
+
+  const onDeleteClick = () => {
+    const del: boolean = window.confirm(
+      '작성된 응원메시지를 삭제하시겠습니까?'
+    );
+    if (del) {
+      try {
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+  return (
+    <div>
+      <Modal show={show} onHide={handleClose} className={styles.test}>
+        <Modal.Header closeButton>
+          <Modal.Title>응원글 수정</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <div style={{ width: '100%', padding: '0px' }}>
+                <Row style={{ margin: '0px' }}>
+                  <div className={styles.group}>
+                    <Col>
+                      <Row>
+                        <Col>
+                          <label
+                            className={styles.form_label}
+                            htmlFor="nickname"
+                          >
+                            닉네임
+                          </label>
+                        </Col>
+                        <Col>
+                          <div>
+                            <input
+                              id="nickname"
+                              type="text"
+                              placeholder="닉네임을 입력해주세요."
+                              maxLength={10}
+                            />
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col>
+                      <Row>
+                        <Col>
+                          <label
+                            className={styles.form_label}
+                            htmlFor="opendate"
+                          >
+                            공개 날짜
+                          </label>
+                        </Col>
+                        <Col>
+                          <div>
+                            <input type="date" id="opendate" />
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </div>
+                </Row>
+
+                <Row style={{ margin: '0px' }}>
+                  <div className={styles.group}>
+                    <Col>
+                      <Row>
+                        <Col>
+                          <label
+                            className={styles.form_label}
+                            htmlFor="password"
+                          >
+                            비밀번호
+                          </label>
+                        </Col>
+                        <Col>
+                          <div>
+                            <input
+                              id="password"
+                              type="password"
+                              placeholder="비밀번호를 입력해주세요."
+                            />
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col>
+                      <Row>
+                        <Col>
+                          <label
+                            className={styles.form_label}
+                            htmlFor="password-check"
+                          >
+                            비밀번호 확인
+                          </label>
+                        </Col>
+                        <Col>
+                          <div>
+                            <input
+                              id="password-check"
+                              type="password"
+                              placeholder="동일한 비밀번호를 입력하세요."
+                            />
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </div>
+                </Row>
+              </div>
+            </div>
+            <br />
+            <div className={styles.cheer_box}>
+              <div className={styles.cheerHeader}>
+                <label className={styles.vertical_lr} htmlFor="cheer-text">
+                  서술형 응원
+                </label>
+                <div>
+                  <textarea
+                    placeholder="응원글을 작성해주세요."
+                    name="cheer-text"
+                    id="cheer-text"
+                    cols={30}
+                    rows={10}
+                  />
+                  <ul>
+                    <li>
+                      <button type="button" onClick={handleClose}>
+                        취소
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className={styles.btn_hover_border_3}
+                        type="button"
+                        onClick={handleClose}
+                      >
+                        응원취소
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default DetailMsg;
--- frontend/src/utils/DefaultData.tsx
+++ frontend/src/utils/DefaultData.tsx
@@ -5,3 +5,12 @@ export const NoteDefaultData = {
   password2: '',
   content: '',
 };
+
+export const NoteDetailData = {
+  nickname: '',
+  content: '',
+  show_date: '',
+  date: '',
+  problem_num: 0,
+  check_num: 0,
+};
