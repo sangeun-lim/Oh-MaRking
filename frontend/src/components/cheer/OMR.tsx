cd88db45c037c790e391c36459050c704f865daa
sjhyun1006 <sjhyun1006@naver.com>
2022-11-10 08:55:37 +0900

    [FE] feat : components

--- frontend/src/components/cheer/OMR.tsx
+++ frontend/src/components/cheer/OMR.tsx
@@ -128,7 +128,7 @@ function OMR(): JSX.Element {
               <div className={`${styles.body} ${styles.bottom}`}>
                 {/* 즐겨찾기 보여주는 부분 */}
 
-                <Carousel>
+                {/* <Carousel>
                   {favoriteList.map((data) => (
                     // <div>
                     <Carousel.Item key={data.noteId}>
@@ -141,55 +141,9 @@ function OMR(): JSX.Element {
                     // </div>
                   ))}
                 </Carousel>
-                {/* isOwner일때 안내사항 */}
-                {/* <UseNotice omrBg={omrBg} isOwner={omr.isOwner} /> */}
-                {/* &#8251; 이름 옆의 링크복사를 통해 다른 사람들에게 응원메시지를
-                  요청해보세요!
-                  <br />
-                  &#8251; 마지막 답안지에서 마킹이 20개 이상일 때, 새로운
-                  답안지를 받을 수 있습니다.
-                  <br />
-                  &#8251; 표기 안내
-                  <br /> */}
-                {/* <div className={`${styles[omrBg[0]]}`}>
-                    작성 가능한 칸입니다.
-                  </div>
-                  <br />
-                  <div className={`${styles[omrBg[1]]}`}>
-                    이미 읽은 칸입니다.
-                  </div>
-                  <br />
-                  <div className={`${styles[omrBg[2]]}`}>
-                    아직 안 읽은 칸입니다.
-                  </div>
-                  <br />
-                  <div className={`${styles[omrBg[3]]}`}>
-                    아직 읽을 수 없는 칸입니다.
-                  </div>
-                  <br />
-                  <div className={`${styles[omrBg[4]]}`}>
-                    좋아요한 칸입니다.
-                  </div>
-                  <br /> */}
-                {/* isOwner가 아닐때 안내사항부분 */}
-                {/*  */}
-                {/* <>
-                  &#8251; 이 수험표는 {user.name}을 위한 응원수험표입니다.
-                  <br />
-                  &#8251; {user.name}을 위한 응원과 격려의 메시지를 작성해보세요! (폭언 및 욕설은 금지!!)
-                  <br />
-                  &#8251; 빈 마킹란을 클릭하면 응원 메시지를 작성할 수 있습니다.
-                  <br />
-                  &#8251; 마지막 답안지에서 마킹이 20개 이상일 때, 새로운 답안지를 받을 수 있습니다.
-                  <br />
-                  &#8251; 표기 안내
-                  <br />
-                  응원 메시지를 작성할 수 있습니다!
-                  <br />
-                  지금 읽을 수 있습니다!
-                  <br />
-                  아직 읽을 수 없습니다!
-                </p> */}
+          */}
+                <UseNotice omrBg={omrBg} isOwner={omr.isOwner} />
+
                 <div>
                   <div className={styles.pallet}>
                     <Pallet colorList={colorList} />
--- frontend/src/components/cheer/UseNotice.tsx
+++ frontend/src/components/cheer/UseNotice.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import type { RootState } from '../../store/store';
-import styles from './UseNotcie.module.scss';
+import styles from './OMR.module.scss';
 
 interface UseNoticeProps {
   omrBg: string[];
