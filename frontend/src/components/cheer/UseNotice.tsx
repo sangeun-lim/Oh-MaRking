a0c2d8af400110ae15e3a1483293ef6b51c602d1
sjhyun1006 <sjhyun1006@naver.com>
2022-11-11 10:46:31 +0900

    [FE] style: notice css

--- frontend/src/components/cheer/UseNotice.tsx
+++ frontend/src/components/cheer/UseNotice.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
+// import { BsClipboardCheck, BsBackspace } from 'react-icons/bs';
 import { useSelector, useDispatch } from 'react-redux';
+import LinkCopy from './LinkCopy';
 import type { RootState } from '../../store/store';
 import styles from './UseNotice.module.scss';
 
@@ -12,11 +14,11 @@ function UseNotice({ omrBg, isOwner }: UseNoticeProps): JSX.Element {
   const { user } = useSelector((state: RootState) => state);
 
   return (
-    <div>
+    <div className={styles.notice_font}>
       {isOwner ? (
         <div>
-          &#8251; 이름 옆의 링크복사를 통해 다른 사람들에게 응원메시지를
-          요청해보세요!
+          &#8251; <LinkCopy />
+          버튼을 눌러 지인들에게 응원받아보세요.
           <br />
           &#8251; 마지막 답안지에서 마킹이 20개 이상일 때, 새로운 답안지를 받을
           수 있습니다.
@@ -25,24 +27,24 @@ function UseNotice({ omrBg, isOwner }: UseNoticeProps): JSX.Element {
           <br />
           <div className={styles.rules}>
             <div className={styles.nubi}>
-              <span className={`${styles[omrBg[0]]}`}>{''}</span>작성 가능한
-              칸입니다.
+              <span className={`${styles[omrBg[0]]}`}>{''}</span>
+              <span className={styles.noticetext}>작성 가능한 칸입니다.</span>
             </div>
             <div className={styles.nubi}>
-              <span className={`${styles[omrBg[1]]}`}>{''}</span>이미 읽은
-              칸입니다.
+              <span className={`${styles[omrBg[1]]}`}>{''}</span>
+              <span>이미 읽은 칸입니다.</span>
             </div>
             <div className={styles.nubi}>
-              <span className={`${styles[omrBg[2]]}`}>{''}</span>아직 안 읽은
-              칸입니다.
+              <span className={`${styles[omrBg[2]]}`}>{''}</span>
+              <span>아직 안 읽은 칸입니다.</span>
             </div>
             <div className={styles.nubi}>
-              <span className={`${styles[omrBg[3]]}`}>{''}</span>아직 읽을 수
-              없는 칸입니다.
+              <span className={`${styles[omrBg[3]]}`}>{''}</span>
+              <span>아직 읽을 수 없는 칸입니다.</span>
             </div>
             <div className={styles.nubi}>
-              <span className={`${styles[omrBg[4]]}`}>{''}</span>좋아요한
-              칸입니다.
+              <span className={`${styles[omrBg[4]]}`}>{''}</span>
+              <span>좋아요한 칸입니다.</span>
             </div>
           </div>
           <br />
