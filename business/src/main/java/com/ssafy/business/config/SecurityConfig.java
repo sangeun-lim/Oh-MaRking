352d68f6baadc585366a1faf71b760ed47e717d4
eodhlwjr97 <eodhlwjr97@gmail.com>
2022-11-11 15:45:57 +0900

    BL -> SecurityConfig Bean 등록해서 수정

--- business/src/main/java/com/ssafy/business/config/SecurityConfig.java
+++ business/src/main/java/com/ssafy/business/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.ssafy.business.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.http.HttpMethod.OPTIONS;
 
 @Configuration
 @RequiredArgsConstructor
@@ -13,15 +21,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+//
+//        http.authorizeRequests()
+//                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll(); // 해당 코드 추가
 
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/**/*").permitAll(); // 해당 코드 추가
 //        http.cors().disable()			//cors 방지
 //                .csrf().disable()			//csrf 방지
 //                .formLogin().disable()		//기본 로그인페이지 없애기
 //                .headers().frameOptions().disable();
+        http
+                .csrf().disable()
+                .cors().configurationSource(corsConfigurationSource()).and()
 
+                .authorizeRequests()
+                .antMatchers("/user/**", "/logic/**").permitAll()
+                .antMatchers(OPTIONS,"/**").permitAll()
+                .and()
+
+                .formLogin().disable();
+
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://127.0.0.1:3000");
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
