2360509297e75ecbb706240962486921105b4818
김대현 <qpwo943@naver.com>
2022-11-11 09:48:16 +0900

    [BE] fix: WebConfig path 추가

--- business/src/main/java/com/ssafy/business/config/WebConfig.java
+++ business/src/main/java/com/ssafy/business/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
         return beanNameViewResolver;
     }
 
-    private static final String[] EXCLUDE_PATHS = {"/omr/**","/note/**","/event/**","/swagger-resources/**","/swagger-ui/**"};
+    private static final String[] EXCLUDE_PATHS = {"/logic/omr/**","/logic/note/**","/logic/event/**","/swagger-resources/**","/swagger-ui/**"};
 
 //    private JwtInterceptor jwtInterceptor;
 //    @Autowired
