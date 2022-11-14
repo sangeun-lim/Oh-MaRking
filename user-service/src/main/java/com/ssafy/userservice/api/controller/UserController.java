8e1182f035c506ea52202d6a486fd00ce9094862
전윤희 <eodhlwjr97@gmail.com>
2022-11-14 14:48:24 +0900

    UserController @CrossOrigin

--- user-service/src/main/java/com/ssafy/userservice/api/controller/UserController.java
+++ user-service/src/main/java/com/ssafy/userservice/api/controller/UserController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
