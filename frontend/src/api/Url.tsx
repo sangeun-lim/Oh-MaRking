b6d308fcfe04b10190048217ce14cf05bbf0be4a
sangeun <lse2625@gmail.com>
2022-11-14 20:12:48 +0900

    [FE] fix: MSA_version_url

--- frontend/src/api/Url.tsx
+++ frontend/src/api/Url.tsx
@@ -1,38 +1,36 @@
-const HOST = 'http://oh-marking.com:8081/api/';
-const HOST2 = 'http://oh-marking.com:8081/logout';
+const HOST = 'https://oh-marking.com';
 
-const USER = 'user/';
-const OMR = 'omr/';
-const OMR2 = 'omr';
-const EVENT = 'event/';
-const NOTE = 'note/';
-const NOTE2 = 'note';
-const CHECK = 'check';
+const USER = '/user';
+const OMR = '/omr';
+const EVENT = '/event';
+const NOTE = '/note';
+const CHECK = '/check';
+const LOGIC = '/logic';
 
 const Url = {
   auth: {
-    getCodedEmail: () => `${HOST + USER}email`,
-    logout: () => `${HOST2}`,
-    updateIntroduction: () => `${HOST}user`,
+    getCodedEmail: () => `${HOST + USER}/email`,
+    logout: () => `${HOST}/logout`,
+    updateIntroduction: () => `${HOST + USER}`,
   },
 
   omr: {
-    LinkAccess: (codedEmail: string) => `${HOST + USER}info/${codedEmail}`,
-    ReadUserOmr: (omrId: number) => `${HOST + OMR + USER}${omrId}`,
-    ReadGuestOmr: (omrId: number) => `${HOST + OMR}guest/${omrId}`,
-    newOMR: () => `${HOST + OMR2}`,
-    changeColor: () => `${HOST + OMR}color`,
-    createOrUpdateOrDeleteNote: (noteId: number) => `${HOST + OMR}${noteId}`,
+    LinkAccess: (codedEmail: string) => `${HOST + USER}/info/${codedEmail}`,
+    ReadUserOmr: (omrId: number) => `${HOST + LOGIC + OMR + USER}/${omrId}`,
+    ReadGuestOmr: (omrId: number) => `${HOST + LOGIC + OMR}/guest/${omrId}`,
+    newOMR: () => `${HOST + LOGIC + OMR}`,
+    changeColor: () => `${HOST + LOGIC + OMR}/color`,
+    createOrUpdateOrDeleteNote: (noteId: number) => `${HOST + OMR}/${noteId}`,
   },
 
   note: {
-    createOrUpdateNote: () => `${HOST + NOTE2}`,
-    DeleteNote: (noteId: number) => `${HOST + NOTE}del/${noteId}`,
-    readUserNote: (noteId: number) => `${HOST + NOTE}${noteId}`,
-    readGuestNote: (noteId: number) => `${HOST + NOTE}guest/${noteId}`,
-    searchNote: (nickname: string) => `${HOST + NOTE}search/${nickname}`,
-    likeNote: () => `${HOST + NOTE}favorite`,
-    likeList: (codedEmail: string) => `${HOST + NOTE}favorites/${codedEmail}`,
+    createOrUpdateNote: () => `${HOST + LOGIC + NOTE}`,
+    DeleteNote: (noteId: number) => `${HOST + LOGIC + NOTE}/del/${noteId}`,
+    readUserNote: (noteId: number) => `${HOST + LOGIC + NOTE}/${noteId}`,
+    readGuestNote: (noteId: number) => `${HOST + LOGIC + NOTE}/guest/${noteId}`,
+    searchNote: (nickname: string) => `${HOST + NOTE}/search/${nickname}`,
+    likeNote: () => `${HOST + NOTE}/favorite`,
+    likeList: (codedEmail: string) => `${HOST + NOTE}/favorites/${codedEmail}`,
   },
   password: {
     checkPW: () => `${HOST + NOTE + CHECK}`,
