adb928d704ee9fc701184a6189008946c95589e2
임상은 <sang9203@naver.com>
2022-11-15 18:01:14 +0900

    [FE] fix: 즐겨찾기 조회 수정

--- frontend/src/api/Url.tsx
+++ frontend/src/api/Url.tsx
@@ -32,7 +32,8 @@ const Url = {
     readGuestNote: (noteId: number) => `${HOST + NOTE}guest/${noteId}`,
     searchNote: (nickname: string) => `${HOST + NOTE}search/${nickname}`,
     likeNote: () => `${HOST + NOTE}favorite`,
-    likeList: (codedEmail: string) => `${HOST + NOTE}favorites/${codedEmail}`,
+    // likeList: (codedEmail: string) => `${HOST + NOTE}favorites/${codedEmail}`,
+    likeList: (omrId: number) => `${HOST + OMR}/favorites/${omrId}`,
   },
   password: {
     checkPW: () => `${HOST + NOTE + CHECK}`,
